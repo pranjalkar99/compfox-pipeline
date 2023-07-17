56d7b93c422acfdb4c5fc467f5ae3e032937e3c3
PRANJAL KAR <74347116+pranjalkar99@users.noreply.github.com>
2023-07-17 21:48:15 +0530

    Yo

--- pipeline_scrap/tests/tests.py
+++ pipeline_scrap/tests/tests.py
@@ -16,3 +16,15 @@ async def test_download_pdf(self):
     assert os.path.exists('pdfs')
     assert len(os.listdir('pdfs')) > 0
     os.rmdir('pdfs')
+@pytest.mark.asyncio
+async def test_remove_directory(self):
+    await start_batch()
+    assert not os.path.exists('pdfs')
+    os.rmdir('temp_output/json_files')
+
+
+# # Tests that the value of the 'message' key is 'Go to /docs for the API documentation.'
+# @pytest.mark.asyncio
+# async def test_message_value(self):
+#     result = await root_message()
+#     assert result['message'] == 'Go to /docs for the API documentation.'
